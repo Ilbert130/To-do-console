c1b1300765ef80037a33717c7fe1eae6c3304b9b
Ilbert130 <ilbertca27@gmail.com>
2023-03-14 03:30:36 +0800

    Creating the function to read and save file

---
+++ helpers/Savefile.js
@@ -0,0 +1,22 @@
+import * as fs from 'node:fs';
+import { join } from 'node:path';
+
+const file = './db/data.json';
+
+//Function to save the file
+const saveDB = (data) => {
+    
+    fs.writeFileSync(file, JSON.stringify(data));
+}
+
+//Function to read the file
+const readDB = () => {
+
+    if(!fs.existsSync(data)){
+        return null;
+    }
+
+    const info = fs.readFileSync(file, {encoding: 'utf-8'});
+    const data = JSON.parse(info);
+    return data;
+}
